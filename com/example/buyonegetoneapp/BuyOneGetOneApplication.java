3e9cd895b4e0029db44cf7b6e2acb30d9c9b620c
puja1608 <42568112+puja1608@users.noreply.github.com>
2024-04-05 19:27:21 +0530

    Create BuyOneGetOneApplication.java

---
+++ com/example/buyonegetoneapp/BuyOneGetOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.buyonegetoneapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyOneGetOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyOneGetOneApplication.class, args);
+	}
+
+}
